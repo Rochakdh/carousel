b33ea3fbaec892d243eff69d54f42ca3e8e3aad2
Rochak Dhakal <rochakdh@gmail.com>
2021-05-10 14:59:54 +0545

    [Added] Interval Set (statically)

--- js/carousel.js
+++ js/carousel.js
@@ -19,3 +19,13 @@ var leftBtn = buttons('left');
 var rightBtn = buttons('right');
 carouselContainer.appendChild(leftBtn)
 carouselContainer.appendChild(rightBtn)
+
+var left = 0
+var interval = setInterval(function(){
+    left = left - 1000
+    if (left<-2000){
+        left = 0
+    }
+    carouselImageWrapper.style.left = left + 'px'
+    console.log(left)
+},1000)
